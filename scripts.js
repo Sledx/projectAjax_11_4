80d2592666d5320d791df7d510b635c323df09be
Figlarz <Figlarz@Figlarzs-MacBook-Pro.local>
2017-03-01 13:07:31 +0100

    task 11.4

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var url = "http://api.icndb.com/jokes/random",
+    button = document.getElementById("get-joke"),
+    paragraph = document.getElementById("joke");
+
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.addEventListener("load", function() {
+        var response = JSON.parse(xhr.response);
+        paragraph.innerText = response.value.joke;
+    });
+    xhr.send();
+}
+paragraph.innerHTML = getJoke(); // zadanie dodatkowe :)
+button.addEventListener("click", function() {
+    getJoke();  
+});
